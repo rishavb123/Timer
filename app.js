06edfe3ff124b00fb32572f90a58c149c502411e
Rishav Bhagat <rishavb123@gmail.com>
2018-12-31 18:12:51 -0500

    Inital Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+let hours;
+let minutes;
+let seconds;
+switch (process.argv.length) {
+    case 3:
+        hours = 0;
+        minutes = 0;
+        seconds = parseInt(process.argv[2]);
+        break;
+    case 4:
+        hours = 0;
+        minutes = parseInt(process.argv[2]);
+        seconds = parseInt(process.argv[3]);
+        break;
+    case 5:
+        hours = parseInt(process.argv[2]);
+        minutes = parseInt(process.argv[3]);
+        seconds = parseInt(process.argv[4]);
+        break;
+    default:
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+}
+
+const totalTime = ((hours * 60 + minutes) * 60 + seconds) * 1000;
+let start = Date.now();
+
+app.use(express.static('public'))
+
+app.get("/time", (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    const t = totalTime - Date.now() + start;
+    res.send("" + ((t < 0) ? 0 : t));
+});
+
+app.listen(port, () => console.log(`Timer listening on port ${port}`));
---
+++ public/script.js
@@ -0,0 +1,46 @@
+function getTime() {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "http://192.168.1.31:4000/time", false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+const span = document.getElementById("time");
+const audio = new Audio('alert.mp3');
+audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    if (timerState == "off")
+        this.play();
+}, false);
+let timerState = "on";
+setInterval(() => {
+    const time = getTime();
+    if (time == 0) {
+        if (timerState == "on") {
+            timerState = "off";
+            audio.play();
+        }
+    } else {
+        timerState = "on";
+    }
+    let ms = Math.floor(time % 1000);
+    let seconds = Math.floor((time / 1000) % 60);
+    let minutes = Math.floor((time / 60000) % 60);
+    let hours = Math.floor((time / 3600000));
+    if ((seconds + "").length < 2)
+        seconds = "0" + seconds;
+    if ((minutes + "").length < 2)
+        minutes = "0" + minutes;
+    switch ((ms + "").length) {
+        case 1:
+            ms = "00" + ms;
+            break;
+        case 2:
+            ms = "0" + ms;
+            break;
+    }
+    if (window.innerHeight > window.innerWidth)
+        span.innerHTML = `${hours}:${minutes}:${seconds}`;
+    else
+        span.innerHTML = `${hours}:${minutes}:${seconds}:${ms}`;
+
+}, 10);
